7b3112d0db1ce019ffbab956170e833760f0e7c2
gpraveendevops <49006380+gpraveendevops@users.noreply.github.com>
2019-03-28 05:00:26 +0530

    Update Node1.Crontab_job.sh

--- Node1.Crontab_job.sh
+++ Node1.Crontab_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat /var/log/auth.log | grep sshd | wc -l > /var/SSHattempts.txt
-scp /var/SSHattempts.txt root@52.14.188.168:/var/output2.txt
+scp /var/SSHattempts.txt root@172.31.41.129:/var/output.txt
